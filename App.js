c93ebd2f020d6ee783d0a0503a68b8ba733eee88
keshi2021 <84689938+keshi2021@users.noreply.github.com>
2021-09-30 18:43:20 -0700

    create map to add-to-list

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView, TextInput } from "react-native";
 
 export default function App() {
+  const [itemCount, setItemCount] = useState(2);
+  const [items, setItems] = useState({
+    1: "first item",
+    2: "second item",
+  });
+  const itemsArray = Object.entries(items);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {itemsArray.map((item, index) => {
+          return (
+            <View key={item[0]} id={item[0]}>
+              <Text>{item[1]}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+
+      {/* Text Input with Button 
+            Uses function that accepts user input task and adds to list
+        */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
